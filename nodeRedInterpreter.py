e5baedb57936b3042fc22f0e390185b5820ef591
Manny-OnGit <manny.fluss@gmail.com>
2023-05-27 19:58:28 -0700

    mouse controlls work

--- nodeRedInterpreter.py
+++ nodeRedInterpreter.py
@@ -2,22 +2,22 @@ import keyboard
 import time
 import re
 import asyncio
-from pynput.mouse import Button, Controller
+import pydirectinput
 
 class constants:
     pollRate = .1
     killSwitch = '/'
     mouseSensitivityMult = 1
-    toggleSwitch = '.'
+    toggleSwitch = '='
     #myPathAlt = "C:/Users/manny/AppData/Roaming/npm/node-red"
 
 command_to_key = {
-"neutral" : {"input":"a","mouseControl":False,"mouseDxDy":(1.0,1.0)},
+"neutral" : {"input":"a","mouseControl":True,"mouseDxDy":(1.0,1.0)},
 "push" : {"input":"b","mouseControl":False,"mouseDxDy":(1.0,1.0)},
 "pull" : {"input":"c","mouseControl":False,"mouseDxDy":(1.0,1.0)},
 "lift" : {"input":"d","mouseControl":False,"mouseDxDy":(1.0,1.0)},
 "drop" : {"input":"e","mouseControl":False,"mouseDxDy":(1.0,1.0)},
-"left" : {"input":"f","mouseControl":False,"mouseDxDy":(1.0,1.0)},
+"left" : {"input":"f","mouseControl":False,"mouseDxDy":(1.0,1.0)}, 
 "right" : {"input":"g","mouseControl":False,"mouseDxDy":(1.0,1.0)},
 "rotateRight" : {"input":"h","mouseControl":False,"mouseDxDy":(1.0,1.0)},
 "rotateLeft" : {"input":"i","mouseControl":False,"mouseDxDy":(1.0,1.0)},
@@ -30,27 +30,31 @@ command_to_key = {
 last_key_pressed = ""
 curr_key_pressed = ""
 last_input_detected = ""
-mouse = Controller()
+enabled = True
 def main():
     myPath = "" 
     with open('myPathInfo.txt', 'r') as file:
         myPath = file.read()
     global curr_key_pressed
     global last_key_pressed
+    global last_input_detected
+    global enabled
     while(True):
         if keyboard.is_pressed(constants.killSwitch):
             break
         if keyboard.is_pressed(constants.toggleSwitch):
+            enabled = not enabled
+            continue
+        if enabled == False:
             time.sleep(constants.pollRate)
             continue
-
         with open(myPath, "r", encoding='utf-16') as f:
             lines = f.readlines()
+            #no new input has been detected:
             if (lines == []) or lines[-1] == last_input_detected:
                 time.sleep(constants.pollRate)
                 continue
-            #no new input has been detected:
-
+            last_input_detected = lines[-1]
             pollInput(lines[-1])
         time.sleep(constants.pollRate)
 
@@ -66,7 +70,6 @@ def pollInput(_line : str):
         return
     matches = re.findall(pattern,_line)
     mouseControl = command_to_key[matches[0][0]]["mouseControl"]
-    #print("mouse control = " +str(mouseControl))
     if mouseControl == True:
         thoughtToMouseMovement(matches[0][0],int(matches[0][1]))
     else:
@@ -75,26 +78,16 @@ def pollInput(_line : str):
 
 def thoughtToMouseMovement(_command, _thinkingIntensity):
     if command_to_key.__contains__(_command):
-        
-        mouse.move(command_to_key[_command]["mouseDxDy"][0] * constants.mouseSensitivityMult * _thinkingIntensity
-                   ,command_to_key[_command]["mouseDxDy"][1] * constants.mouseSensitivityMult * _thinkingIntensity)
+        movX = command_to_key[_command]["mouseDxDy"][0] * constants.mouseSensitivityMult * _thinkingIntensity
+        movY = command_to_key[_command]["mouseDxDy"][1] * constants.mouseSensitivityMult * _thinkingIntensity
+        pydirectinput.moveRel(movX,movY)
 
 def thoughtToKeyPress(_command):
     global curr_key_pressed
     global last_key_pressed
     if command_to_key.__contains__(_command):
-        #print("pressing key " + command_to_key[_command]["input"])
         asyncio.run(press_and_release_time(command_to_key[_command]["input"],constants.pollRate))
         pass
-        # if last_key_pressed != curr_key_pressed:
-        #     if curr_key_pressed != "":
-        #         keyboard.press(curr_key_pressed)
-        #     if last_key_pressed != "":
-        #         keyboard.release(last_key_pressed)
-        # last_key_pressed = curr_key_pressed        
-        # curr_key_pressed = command_to_key[_command]["input"]
-
-    #keyboard.press_and_release(command_to_key[_command])
 
 async def press_and_release_time(key,_holdTime):
     print(key)
